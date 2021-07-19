96c58ae99280f0020610e7f7ce8563d3be4311c4
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-07-19 09:52:49 +0100

    adds terraform config

---
+++ main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.68.0"
+    }
+  }
+  backend "azurerm" {
+        resource_group_name  = azurerm_resource_group.rg.name
+        storage_account_name = "demo-state"
+        container_name       = "tstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "rg"{
+	name = "demo-vm"
+	location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+	name = "demo-vm-net"
+	address_space = ["10.0.0.0/16"]
+	location  = azurerm_resource_group.rg.location
+	resource_group_name = azurerm_resource_group.rg.name
+	
+}
+
+resource "azurerm_subnet" "snet" {
+	name                 = "internal"
+  	resource_group_name  = azurerm_resource_group.rg.name
+  	virtual_network_name = azurerm_virtual_network.vnet.name
+  	address_prefixes     = ["10.0.2.0/24"]
+
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "demo-vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.snet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "demo-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.size
+  admin_username      = var.username
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+output "instance_ip" {
+	value = azurerm_linux_virtual_machine.vm.public_ip_address
+}
