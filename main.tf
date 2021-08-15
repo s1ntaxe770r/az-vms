f26fca93c5267bfe4a185ab745ce2c3263ef4da4
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-08-15 13:18:12 +0100

    mods main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.68.0"
+      version = "=2.46.0"
     }
   }
-
 }
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 
@@ -72,8 +77,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "18.04-LTS"
     version   = "latest"
   }
-}
-
-output "instance_ip" {
-  value = azurerm_linux_virtual_machine.vm.public_ip_address
 }
