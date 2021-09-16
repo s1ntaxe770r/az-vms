548f9a3f50c0018ff1c02a57b2f47ab3f20cd8b5
s1ntaxe770r <53065463+s1ntaxe770r@users.noreply.github.com>
2021-09-16 15:02:29 +0100

    trigger destroy

--- main.tf
+++ main.tf
@@ -1,90 +1,90 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=2.46.0"
+#     }
+#   }
+# }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-}
-
-
-resource "azurerm_resource_group" "rg" {
-  name     = "demo-vm1"
-  location = "West Europe"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "demo-vm-net"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-}
-
-resource "azurerm_subnet" "snet" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-
-}
-resource "azurerm_public_ip" "ip" {
-  name                = "pub"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  allocation_method   = "Static"
-}
+# # Configure the Microsoft Azure Provider
+# provider "azurerm" {
+#   features {}
+#   subscription_id = var.subscription_id
+#   client_id       = var.client_id
+#   client_secret   = var.client_secret
+#   tenant_id       = var.tenant_id
+# }
 
 
-resource "azurerm_network_interface" "nic" {
-  name                = "demo-vm-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_resource_group" "rg" {
+#   name     = "demo-vm1"
+#   location = "West Europe"
+# }
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.snet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.ip.id
-  }
-}
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "demo-vm-net"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "demo-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = var.size
-  admin_username      = var.username
-  admin_password =  var.admin_pass
-  disable_password_authentication = false
-  network_interface_ids = [
-    azurerm_network_interface.nic.id,
-  ]
+# }
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+# resource "azurerm_subnet" "snet" {
+#   name                 = "internal"
+#   resource_group_name  = azurerm_resource_group.rg.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefixes     = ["10.0.2.0/24"]
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-}
+# }
+# resource "azurerm_public_ip" "ip" {
+#   name                = "pub"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   allocation_method   = "Static"
+# }
 
-data "azurerm_public_ip" "ip" {
-  name                = azurerm_public_ip.ip.name
-  resource_group_name = azurerm_linux_virtual_machine.vm.resource_group_name
-}
+
+# resource "azurerm_network_interface" "nic" {
+#   name                = "demo-vm-nic"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.snet.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.ip.id
+#   }
+# }
+
+# resource "azurerm_linux_virtual_machine" "vm" {
+#   name                = "demo-vm"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   size                = var.size
+#   admin_username      = var.username
+#   admin_password =  var.admin_pass
+#   disable_password_authentication = false
+#   network_interface_ids = [
+#     azurerm_network_interface.nic.id,
+#   ]
+
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "UbuntuServer"
+#     sku       = "18.04-LTS"
+#     version   = "latest"
+#   }
+# }
+
+# data "azurerm_public_ip" "ip" {
+#   name                = azurerm_public_ip.ip.name
+#   resource_group_name = azurerm_linux_virtual_machine.vm.resource_group_name
+# }
 
